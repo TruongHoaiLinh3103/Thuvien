dc16106a942750354a59595cd60cf9f01b9dd41f
Anikey <eagleremlinh@gmail.com>
2023-12-14 10:21:51 +0700

    B

--- src/layouts/ContactDetail.js
+++ src/layouts/ContactDetail.js
@@ -30,7 +30,7 @@ const ContactDetail = () => {
                             <input type='tel' placeholder='Số điện thoại(*)' ref={Tel} onChange={() => {console.log(Tel.current.value)}}/>
                             <input type='text' placeholder='Tiêu đề(*)' ref={Title} onChange={() => {console.log(Title.current.value)}}/>
                         </div>
-                        <CKEditor
+                        {/* <CKEditor
                             editor={ ClassicEditor }
                             data={Content}
                             onReady={ editor => {
@@ -43,7 +43,7 @@ const ContactDetail = () => {
                                 setContent(...Content, data)
                                 console.log(Content)
                             } }
-                        />
+                        /> */}
                         <div className='ContactDetail-body-Item-btn'>
                             <button>Gửi</button>
                         </div>
