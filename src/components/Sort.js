8309801fb4192161fce97d3469e358a7c2211879
Anikey <eagleremlinh@gmail.com>
2024-07-26 22:55:50 +0700

    L

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -7,28 +7,28 @@ const Sort = ({ handleSort, handleList }) => {
         <div className='Sort'>
             <button className='Sort-btn' onClick={() => handleSort("&_sort=id&_order=desc")}>Mới Nhất</button>
             <span>|</span>
-            <button className='Sort-btn' onClick={() => handleSort("&_sort=rating&_order=desc")}>Đánh giá cao</button>
+            <button className='Sort-btn' onClick={() => handleSort("&_sort=rating&_order=desc")}>Đánh Giá Cao</button>
             <span>|</span>
             <button className='Sort-btn' onClick={() => setSort(true)}>Thể Loại</button>
             <div className='Sort-dash' style={{display: !sort ? "none" : "flex"}} onClick={() => setSort(false)}>
                 <div className='Sort-dash-list'>
-                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Kinh_dị")}>Kinh dị</button>
+                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Kinh-dị")}>Kinh dị</button>
                     <span>|</span>
-                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Xuyên_không")}>Xuyên không</button>
+                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Phiêu-lưu")}>Phiêu lưu</button>
                     <span>|</span>
-                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Ngôn_tình")}>Ngôn tình</button>
+                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Ngôn-tình")}>Ngôn tình</button>
                     <span>|</span>
-                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Hiện_đại")}>Hiện đại</button>
+                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Mưu-sự")}>Mưu sự</button>
                     <span>|</span>
-                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Cổ_đại")}>Cổ đại</button>
+                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Dị-giới")}>Dị giới</button>
                     <span>|</span>
-                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Truyện_18")}>Truyện 18+</button>
+                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Truyện-18")}>Truyện 18+</button>
                     <span>|</span>
                     <button className='-dash-list_btn' onClick={() => handleList("&list_like=Phim")}>Phim</button>
                     <span>|</span>
-                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Trinh_thám")}>Trinh thám</button>
+                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Trinh-thám")}>Trinh thám</button>
                     <span>|</span>
-                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Thế_giới_khác")}>Thế giới khác</button>
+                    <button className='-dash-list_btn' onClick={() => handleList("&list_like=Truyện-cười")}>Truyện cười</button>
                 </div>
             </div>
         </div>
